90bd2779060b054b7b6172b0e1165d5e18512838
ColineVL <coline.vanleeuwen@gmail.com>
2022-06-06 15:29:28 +0200

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   get 'site/home'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
